4e739742d37b92dc85a274a18db6e04ba53e1d72
Nmacroy <nmacroy@gmail.com>
2020-02-15 23:51:46 -0600

    Users Router

--- exercise-tracker/backend/server.js
+++ exercise-tracker/backend/server.js
@@ -16,6 +16,11 @@ connection.once('open', () => {
     console.log("MongoDB database connection established successfully");
 });
 
+const exerciseRouter = require('./routes/exercise');
+const usersRouter = require('./routes/users');
+
+app.use('/exercise', exerciseRouter);
+app.use('/users', usersRouter);
 
 app.listen(port, () => {
     console.log(`Server is running on port: ${port}`);
