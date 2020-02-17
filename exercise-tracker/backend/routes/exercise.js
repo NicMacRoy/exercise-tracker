d818599cf4f6bf09b3ad4426632b3779168028aa
Nicholas MacRoy <nmacroy@gmail.com>
2020-02-16 23:26:03 -0600

    Final Commit

--- exercise-tracker/backend/routes/exercise.js
+++ exercise-tracker/backend/routes/exercise.js
@@ -3,9 +3,9 @@ let Exercise = require('../models/exercise.model');
 
 router.route('/').get((req, res) => {
     Exercise.find()
-    .then(exercises => res.json(exercises))
-    .catch(err => res.status(400).json('ERROR: ' +err));
-});
+      .then(exercises => res.json(exercises))
+      .catch(err => res.status(400).json('Error: ' + err));
+  });
 
 
 router.route('/add').post((req,res) => {
